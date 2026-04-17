9690edffb0310b414b6ff6ae54e3c5cce50bb45d
Kouadio Alfred <alfred.justbelieve.5@gmail.com>
2026-04-17 21:49:54 +0000

    add ProtectedRoutes component for managing route permissions

---
+++ src/context/permission/Permission.tsx
@@ -0,0 +1,14 @@
+import type { JSX } from "react";
+
+type ProtectedRouteProps = {
+  permission: string;
+  children: React.ReactNode;
+};
+
+export const ProtectedRoutes = ({ permission, children }: ProtectedRouteProps) => {
+
+ 
+
+
+};
+
