2979a7871c8f1713b69ed03360dd84fb64c0afe0
Kouadio Alfred <alfred.justbelieve.5@gmail.com>
2026-04-18 13:35:34 +0000

    add Exercice3 component with generic Select functionality

---
+++ src/composantGenerics/Exercice3.tsx
@@ -0,0 +1,9 @@
+//**Enonnce */
+
+/**
+ * 
+Select générique
+Moyen generics keyof
+Crée un composant Select<T> qui reçoit une liste d'options options: T[], deux clés valueKey: keyof T et labelKey: keyof T, et un callback onChange: (value: T) => void. Le composant affiche un <select> HTML dont les options sont construites depuis le tableau.
+valueKey: keyof T; labelKey: keyof T
+ */
