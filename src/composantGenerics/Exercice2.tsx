c0870b3993bc61ce3aee5a7789d02cc705c80394
Kouadio Alfred <alfred.justbelieve.5@gmail.com>
2026-04-18 13:09:41 +0000

    add List component interface and initial function definition

--- src/composantGenerics/Exercice2.tsx
+++ src/composantGenerics/Exercice2.tsx
@@ -7,4 +7,14 @@ Facile generics array
 Crée un composant List<T> qui reçoit items: T[] et renderItem: (item: T) => React.ReactNode. Il parcourt les items et délègue le rendu de chacun à renderItem. C'est le même principe que le render du tableau — mais pour une liste.
  */
 
+interface ListProps<T> {
+  items: T[];
+  renderItem: (item: T) => React.ReactNode;
+  emptyMessage: string;
+  getKey: (item: T) => string | number;
+}
 
+
+export default function List<T>({emptyMessage , getKey }:ListProps<T>){
+
+}
