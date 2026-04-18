71d1b9caff91f2002e61be93b78766474820ca6a
Kouadio Alfred <alfred.justbelieve.5@gmail.com>
2026-04-18 17:16:06 +0000

    feat: add RenderExercice3 component for country selection using CustumSelect

---
+++ src/composantGenerics/use/Use3.tsx
@@ -0,0 +1,28 @@
+import CustumSelect from "../Exercice3";
+
+interface Pays {
+  code: string;
+  label: string;
+}
+
+const pays: Pays[] = [
+  {
+    code: "CIV",
+    label: "Cote d'Ivoire",
+  },
+];
+
+export default function RenderExercice3() {
+  return (
+    <div>
+      <CustumSelect
+        options={pays}
+        getKey={(pays) => pays.code}
+        labelKey="label"
+        valueKey="code"
+        placeholder="Selectionner un pays"
+        onChange={(pays) => console.log(pays)}
+      />
+    </div>
+  );
+}
