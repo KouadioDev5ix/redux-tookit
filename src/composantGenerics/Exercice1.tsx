a4c23f7d60421cdecc31022035e5dc2bad0d248a
Kouadio Alfred <alfred.justbelieve.5@gmail.com>
2026-04-18 12:50:38 +0000

    add BoxWrapper component for rendering labeled values

---
+++ src/composantGenerics/Exercice1.tsx
@@ -0,0 +1,28 @@
+interface BoxWrapperProps<T> {
+  value: T;
+  label: string;
+}
+
+export default function BoxWrapper<T>({ label, value }: BoxWrapperProps<T>) {
+  /**
+   *
+   * @param value
+   * @returns
+   */
+  const renderValue = (value: any) => {
+    if (typeof value === "object" && value !== null) {
+      return <pre>{JSON.stringify(value, null, 2)}</pre>;
+    }
+
+    return value;
+  };
+  return (
+    <div className="border border-gray-100 rounded-xl ">
+      <div>
+        {label && <h1 className="font-bold text-gray-800 text-lg">{label}</h1>}
+      </div>
+
+      <p className="font-medium">{renderValue(value)}</p>
+    </div>
+  );
+}
