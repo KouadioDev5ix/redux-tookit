e69bbe2024ba51dca1d9787c9735b75bf6b38efc
Kouadio Alfred <alfred.justbelieve.5@gmail.com>
2026-04-18 12:52:06 +0000

    add Exercice2 component with generic list description

---
+++ src/composantGenerics/Exercice2.tsx
@@ -0,0 +1,8 @@
+//**Enonce  */
+
+/**
+ * 
+Liste générique
+Facile generics array
+Crée un composant List<T> qui reçoit items: T[] et renderItem: (item: T) => React.ReactNode. Il parcourt les items et délègue le rendu de chacun à renderItem. C'est le même principe que le render du tableau — mais pour une liste.
+ */
