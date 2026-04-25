fe6dde39d7917c64076802b8dc45847efc30e1de
Kouadio Alfred <alfred.justbelieve.5@gmail.com>
2026-04-25 22:06:39 +0000

    feat: remove students data from Ajouter component

--- src/views/Ajouter.tsx
+++
@@ -1,9 +0,0 @@
-export const students = {
-  id: 1,
-  name: "Kouadio",
-  prenom: "Konan Patrick",
-  class: "3eme",
-  age: 18,
-  moyenne: { math: 12, anglais: 15, français: 12, svt: 17 },
-  doublant: "Non",
-};
